d5a9c04a567e97b24bd5d1f2244fd86c15e5182e
Andrew Lisin <andrey.lisin@gmail.com>
2015-05-04 23:57:19 +0600

    Add deletion helper functions

--- taskwarrior-mode.el
+++ taskwarrior-mode.el
@@ -15,13 +15,25 @@
   (interactive "sTask description: ")
   (shell-command (format "task add %s" task-description)))
 
-(defun delete-task (task-number)
-  (shell-command (format "task %i delete" task-number)))
+(defun filter-spaces (seq)
+  "Filter spaces from seq of strings"
+  (delq nil (mapcar (lambda (x) (if (string-equal x " ") nil x)) seq)))
+
+(defun get-task-number (line)
+  (let ((tokens (split-string line)))
+    (nth 0 (filter-spaces tokens))))
+
+(defun delete-task ()
+  (interactive)
+  (let ((current-line (thing-at-point 'line)))
+    (let ((task-number (get-task-number current-line)))
+      (message (format "task %i delete" (string-to-number task-number))))))
 
 (define-minor-mode taskwarrior-mode
   "Taskwarrior mode."
   :keymap '(((kbd "g") . display-tasks)
-            ((kbd "a") . add-task)))
+            ((kbd "a") . add-task)
+            ((kbd "d") . delete-task)))
 
 (provide 'taskwarrior-mode)
 ;;; taskwarrior-mode.el ends here
